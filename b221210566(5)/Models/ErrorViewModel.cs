77a055841e40d8ec04c2a988f435342a6816c1eb
Sara Amr <amrwael422@gmail.com>
2024-12-19 20:07:21 +0300

    Add project files.

---
+++ b221210566(5)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace b221210566_5_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
